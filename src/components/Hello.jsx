1246436d530979dea37c0754b9edb6022518ca3a
tani <tani@1-10.com>
2021-05-04 09:38:56 +0900

    Update React.memo

---
+++ src/components/Hello.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Hello = React.memo(() => {
+  return <div>Hello memo</div>
+})
+
+export default Hello
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import Hello from '../components/Hello'
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head>
@@ -9,7 +10,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>test</main>
+      <main>
+        <Hello />
+      </main>
     </div>
   )
 }
+
+export default Home
